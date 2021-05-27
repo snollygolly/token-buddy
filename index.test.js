c7efb2b2278511d65c09cad513a960fb04f560be
Andrew Nguyen <andrew.nguyen@auth0.com>
2021-05-27 13:15:11 -0400

    cleanup tests

--- index.test.js
+++ index.test.js
@@ -2,7 +2,13 @@ const bip39 = require("bip39");
 const Web3 = require("web3");
 const { hdkey } = require("ethereumjs-wallet");
 
-const { init, newAccount, getBalance, getTokenBalance, sendTokens } = require("./index");
+const {
+  init,
+  newAccount,
+  getBalance,
+  getTokenBalance,
+  sendTokens,
+} = require("./index");
 
 jest.mock("bip39", () => ({
   generateMnemonic: jest.fn(),
@@ -39,10 +45,11 @@ const mockInitData = {
       },
     ],
   },
-  mnemonic: "cat dog waffle perrier apple diamond hands unit testing this js module",
+  mnemonic:
+    "cat dog waffle perrier apple diamond hands unit testing this js module",
 };
 
-const mockAddressString = "0x12345";
+const mockAddress = "0x12345";
 
 describe("Root module test", () => {
   beforeEach(() => {
@@ -55,153 +62,98 @@ describe("Root module test", () => {
       await expect(init(undefined)).rejects.toThrowError(expectedError);
     });
     it("Throws a no wallet index error if index is not a number", async () => {
-      const data = {
-        index: null,
+      const missingIndex = {
+        ...mockInitData,
+        index: undefined,
       };
 
       const expectedError = new Error("No wallet index provided");
-      await expect(init(data)).rejects.toThrowError(expectedError);
+      await expect(init(missingIndex)).rejects.toThrowError(expectedError);
     });
     it("Throws a missing provider error if provider is falsey", async () => {
-      const data = {
-        index: 1,
+      const missingProvider = {
+        ...mockInitData,
+        provider: undefined,
       };
 
       const expectedError = new Error("No WS provider supplied");
-      await expect(init(data)).rejects.toThrowError(expectedError);
+      await expect(init(missingProvider)).rejects.toThrowError(expectedError);
     });
     it("Throws a missing token error if token is falsey", async () => {
-      const data = {
-        index: 1,
-        provider: "HTTP://127.0.0.1:7545",
+      const missingToken = {
+        ...mockInitData,
+        token: undefined,
       };
 
       const expectedError = new Error("No token JSON supplied");
-      await expect(init(data)).rejects.toThrowError(expectedError);
+      await expect(init(missingToken)).rejects.toThrowError(expectedError);
     });
     it("Throws a missing token abi error if token abi is not supplied", async () => {
-      const data = {
-        index: 1,
-        provider: "HTTP://127.0.0.1:7545",
+      const misingTokenAbi = {
+        ...mockInitData,
         token: {
-          contract_address: "0x123456789",
+          contract_address: mockInitData.token.contract_address,
         },
       };
 
       const expectedError = new Error("No token JSON ABI supplied");
-      await expect(init(data)).rejects.toThrowError(expectedError);
+      await expect(init(misingTokenAbi)).rejects.toThrowError(expectedError);
     });
     it("Throws a missing token contact_address error if token contract_address is not supplied", async () => {
-      const data = {
-        index: 1,
-        provider: "HTTP://127.0.0.1:7545",
+      const missingTokenContactAddress = {
+        ...mockInitData,
         token: {
-          abi: [
-            {
-              constant: true,
-              inputs: [],
-              name: "mintingFinished",
-              outputs: [
-                {
-                  name: "",
-                  type: "bool",
-                },
-              ],
-              payable: false,
-              stateMutability: "view",
-              type: "function",
-            },
-          ],
+          ...mockInitData.token,
+          contract_address: undefined,
         },
       };
 
       const expectedError = new Error(
         "No token JSON contract address provided"
       );
-      await expect(init(data)).rejects.toThrowError(expectedError);
+      await expect(init(missingTokenContactAddress)).rejects.toThrowError(
+        expectedError
+      );
     });
     it("Generates a new mnemonic if input mnemonic is not provided", async () => {
-      const data = {
-        index: 1,
-        provider: "HTTP://127.0.0.1:7545",
-        token: {
-          contract_address: "0x123456789",
-          abi: [
-            {
-              constant: true,
-              inputs: [],
-              name: "mintingFinished",
-              outputs: [
-                {
-                  name: "",
-                  type: "bool",
-                },
-              ],
-              payable: false,
-              stateMutability: "view",
-              type: "function",
-            },
-          ],
-        },
+      const missingMnemonic = {
+        ...mockInitData,
+        mnemonic: undefined,
       };
 
-      await init(data);
+      await init(missingMnemonic);
       expect(bip39.generateMnemonic).toHaveBeenCalled();
     });
     it("Does not generate a new mnemonic if input mnemonic is provided", async () => {
-      const data = {
-        index: 1,
-        provider: "HTTP://127.0.0.1:7545",
-        token: {
-          contract_address: "0x123456789",
-          abi: [
-            {
-              constant: true,
-              inputs: [],
-              name: "mintingFinished",
-              outputs: [
-                {
-                  name: "",
-                  type: "bool",
-                },
-              ],
-              payable: false,
-              stateMutability: "view",
-              type: "function",
-            },
-          ],
-        },
-        mnemonic:
-          "cat dog waffle perrier apple diamond hands unit testing this js module",
-      };
-
-      await init(data);
+      await init(mockInitData);
       expect(bip39.generateMnemonic).not.toHaveBeenCalled();
     });
   });
+
   describe("newAccount()", () => {
     beforeEach(async () => {
       hdkey.fromMasterSeed.mockReturnValueOnce({
         derivePath: jest.fn().mockReturnValueOnce({
           getWallet: jest.fn().mockReturnValueOnce({
-            getAddressString: jest.fn().mockReturnValueOnce(mockAddressString),
+            getAddressString: jest.fn().mockReturnValueOnce(mockAddress),
           }),
         }),
       });
 
       await init(mockInitData);
-    })
+    });
     it("Returns the wallets address", async () => {
       const address = newAccount();
-      expect(address).toEqual(mockAddressString);
+      expect(address).toEqual(mockAddress);
     });
   });
+
   describe("getBalance", () => {
     beforeEach(async () => {
       hdkey.fromMasterSeed.mockReturnValueOnce({
         derivePath: jest.fn().mockReturnValueOnce({
           getWallet: jest.fn().mockReturnValueOnce({
-            getAddressString: jest.fn().mockReturnValueOnce(mockAddressString),
+            getAddressString: jest.fn().mockReturnValueOnce(mockAddress),
           }),
         }),
       });
@@ -213,19 +165,20 @@ describe("Root module test", () => {
       });
 
       await init(mockInitData);
-    })
+    });
 
     it("Returns the balance of the address", async () => {
-      const balance = await getBalance(mockAddressString);
+      const balance = await getBalance(mockAddress);
       expect(balance).toEqual(10000);
     });
   });
+
   describe("getTokenBalance", () => {
     beforeEach(async () => {
       hdkey.fromMasterSeed.mockReturnValueOnce({
         derivePath: jest.fn().mockReturnValueOnce({
           getWallet: jest.fn().mockReturnValueOnce({
-            getAddressString: jest.fn().mockReturnValueOnce(mockAddressString),
+            getAddressString: jest.fn().mockReturnValueOnce(mockAddress),
           }),
         }),
       });
@@ -236,177 +189,93 @@ describe("Root module test", () => {
           Contract: jest.fn().mockReturnValueOnce({
             methods: {
               balanceOf: jest.fn().mockReturnThis(),
-              call: jest.fn().mockReturnValueOnce(5000)
+              call: jest.fn().mockReturnValueOnce(5000),
             },
           }),
         },
       });
 
       await init(mockInitData);
-    })
+    });
     it("Returns the token balance", async () => {
-      const tokenBalance = await getTokenBalance(mockAddressString);
+      const tokenBalance = await getTokenBalance(mockAddress);
       expect(tokenBalance).toEqual(5000);
     });
   });
+
   describe("sendTokens", () => {
-    it("Generates a contract using the token from the config", async () => {
-      // Populate global config
-      const data = {
-        index: 1,
-        provider: "HTTP://127.0.0.1:7545",
-        token: {
-          contract_address: "0x123456789",
-          abi: [
-            {
-              constant: true,
-              inputs: [],
-              name: "mintingFinished",
-              outputs: [
-                {
-                  name: "",
-                  type: "bool",
-                },
-              ],
-              payable: false,
-              stateMutability: "view",
-              type: "function",
-            },
-          ],
-        },
-        mnemonic:
-          "cat dog waffle perrier apple diamond hands unit testing this js module",
-      };
+    let mockEthContract;
+    let mockEth;
 
-      const mockAddressString = "0x12345";
+    const fromAddress = "0x12345";
+    const toAddress = "0x67890";
+    const amount = 1000;
 
+    beforeEach(async () => {
       hdkey.fromMasterSeed.mockReturnValueOnce({
         derivePath: jest.fn().mockReturnValueOnce({
           getWallet: jest.fn().mockReturnValueOnce({
-            getAddressString: jest.fn().mockReturnValueOnce(mockAddressString),
+            getAddressString: jest.fn().mockReturnValueOnce(mockAddress),
             privateKey: {
-              toString: jest.fn().mockReturnValueOnce('hello')
-            }
+              toString: jest.fn().mockReturnValueOnce("hello"),
+            },
           }),
         }),
       });
 
-      const mockEth = {
-        getBalance: jest.fn().mockResolvedValueOnce(10000),
-        eth: {
-          Contract: jest.fn().mockReturnValueOnce({
-            methods: {
-              balanceOf: jest.fn().mockReturnThis(),
-              call: jest.fn().mockReturnValueOnce(5000),
-              transfer: jest.fn().mockReturnThis(),
-              estimateGas: jest.fn().mockReturnValueOnce(1.000),
-              send: jest.fn().mockReturnValueOnce("whatever")
-            },
-          }),
-          accounts: {
-            privateKeyToAccount: jest.fn().mockReturnValueOnce(mockAddressString),
-            wallet: {
-              create: jest.fn(),
-              add: jest.fn(),
-            }
-          }
-        },
-      };
-
-      Web3.mockReturnValueOnce(mockEth)
-
-      await init(data);
-      newAccount();
-
-      await sendTokens('0x12345', '0x67890', 1000);
-
-      expect(mockEth.eth.Contract).toHaveBeenCalledWith(
-        data.token.abi,
-        data.token.contract_address,{
-          from: '0x12345'
-        }
-      )
-      expect(mockEth.eth.accounts.wallet.create).toHaveBeenCalled();
-      expect(mockEth.eth.accounts.wallet.add).toHaveBeenCalled();
-    });
-    it("Transfers the amount to the specified address", async () => {
-      // Populate global config
-      const data = {
-        index: 1,
-        provider: "HTTP://127.0.0.1:7545",
-        token: {
-          contract_address: "0x123456789",
-          abi: [
-            {
-              constant: true,
-              inputs: [],
-              name: "mintingFinished",
-              outputs: [
-                {
-                  name: "",
-                  type: "bool",
-                },
-              ],
-              payable: false,
-              stateMutability: "view",
-              type: "function",
-            },
-          ],
-        },
-        mnemonic:
-          "cat dog waffle perrier apple diamond hands unit testing this js module",
-      };
-
-      const mockAddressString = "0x12345";
-
-      hdkey.fromMasterSeed.mockReturnValueOnce({
-        derivePath: jest.fn().mockReturnValueOnce({
-          getWallet: jest.fn().mockReturnValueOnce({
-            getAddressString: jest.fn().mockReturnValueOnce(mockAddressString),
-            privateKey: {
-              toString: jest.fn().mockReturnValueOnce('hello')
-            }
-          }),
-        }),
-      });
-
-      const mockEthContract = {
+      mockEthContract = {
         methods: {
-          balanceOf: jest.fn().mockReturnValueOnce({
-            call: jest.fn().mockReturnValueOnce(5000),
+          balanceOf: jest.fn().mockReturnValue({
+            call: jest.fn().mockReturnValue(5000),
           }),
           transfer: jest.fn().mockReturnThis(),
-          estimateGas: jest.fn().mockReturnValueOnce(1.000),
-          send: jest.fn().mockReturnValueOnce("whatever")
+          estimateGas: jest.fn().mockReturnValue(1.0),
+          send: jest.fn().mockReturnValue("whatever"),
         },
-      }
+      };
 
-      const mockEth = {
+      mockEth = {
         getBalance: jest.fn().mockResolvedValueOnce(10000),
         eth: {
           Contract: jest.fn().mockReturnValueOnce(mockEthContract),
           accounts: {
-            privateKeyToAccount: jest.fn().mockReturnValueOnce(mockAddressString),
+            privateKeyToAccount: jest.fn().mockReturnValueOnce(mockAddress),
             wallet: {
               create: jest.fn(),
               add: jest.fn(),
-            }
-          }
+            },
+          },
         },
       };
 
-      Web3.mockReturnValueOnce(mockEth)
-
-      await init(data);
+      Web3.mockReturnValueOnce(mockEth);
+      await init(mockInitData);
       newAccount();
+    });
+    it("Generates a contract using the token from the config", async () => {
+      await sendTokens(fromAddress, toAddress, amount);
 
-      await sendTokens('0x12345', '0x67890', 1000);
+      expect(mockEth.eth.Contract).toHaveBeenCalledWith(
+        mockInitData.token.abi,
+        mockInitData.token.contract_address,
+        {
+          from: fromAddress,
+        }
+      );
+      expect(mockEth.eth.accounts.wallet.create).toHaveBeenCalled();
+      expect(mockEth.eth.accounts.wallet.add).toHaveBeenCalled();
+    });
+    it("Transfers the amount to the specified address", async () => {
+      await sendTokens(fromAddress, toAddress, amount);
 
       expect(mockEthContract.methods.estimateGas).toHaveBeenCalled();
-      expect(mockEthContract.methods.transfer).toHaveBeenCalledWith('0x67890', 1000);
+      expect(mockEthContract.methods.transfer).toHaveBeenCalledWith(
+        toAddress,
+        amount
+      );
       expect(mockEthContract.methods.send).toHaveBeenCalledWith({
-        from: '0x12345',
-        gas: 1.000,
+        from: fromAddress,
+        gas: 1.0,
         value: 0x0,
       });
     });
